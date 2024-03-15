40615d212c5b640855c9af10fca5179661d6044f
fullm <matoma1_@naver.com>
2024-03-15 14:37:21 +0900

    20240315 엑셀에 저장하는 로직

--- src/main/java/kr/excel/example/ExcelWriter.java
+++ src/main/java/kr/excel/example/ExcelWriter.java
@@ -1,7 +1,14 @@
 package kr.excel.example;
 
 import kr.excel.entity.MemberVO;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -33,5 +40,37 @@ public class ExcelWriter {
         }
         sc.close();
 
+        try{
+            //엑셀과 관련된 라이브러리
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet("회원 정보");
+
+            //header
+            Row headerrow = sheet.createRow(0);
+            headerrow.createCell(0).setCellValue("name");
+            headerrow.createCell(1).setCellValue("age");
+            headerrow.createCell(2).setCellValue("birthday");
+            headerrow.createCell(3).setCellValue("married");
+
+            for(int i=0; i< members.size(); i++){
+                MemberVO member = members.get(i);
+                Row row = sheet.createRow(i +1);
+                row.createCell(0).setCellValue(member.getName());
+                row.createCell(1).setCellValue(member.getAge());
+                row.createCell(2).setCellValue(member.getBirthday());
+                Cell marriedCell = row.createCell(5);
+                marriedCell.setCellValue(member.isMarried());
+            }
+
+            //엑셀 파일 저장
+            String filename = "members.xlsx";
+            FileOutputStream outputStream = new FileOutputStream(new File(filename));
+            workbook.write(outputStream);
+            workbook.close();
+            System.out.println("엑셀 파일 저장 되었습니다." + filename);
+        }catch (IOException e){
+            System.out.println("엑셀 파일 저장 중 오류 발생");
+            e.printStackTrace();
+        }
     }
 }
